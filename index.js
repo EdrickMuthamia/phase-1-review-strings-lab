9699269efc6a7c1df5608176c6e8cd3c6e55e12e
EdrickMuthamia <edrickmuthamia05@gmail.com>
2025-06-04 17:44:32 +0300

    Implement welcome messages for the current user

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Edrick'; // Replace 'Your Name' with any name you like
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+console.log(welcomeMessage);
+console.log(excitedWelcomeMessage);
+console.log(shortGreeting);
